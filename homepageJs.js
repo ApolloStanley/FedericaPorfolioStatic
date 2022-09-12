2834691da54907bccff3d8a40f46ab4c9a004295
Matthew <matthew.estanley1996@gmail.com>
2022-09-12 20:39:14 +0100

    added image slider on the front page

--- homepageJs.js
+++ homepageJs.js
@@ -4,6 +4,15 @@ function showMenu() {
     navlinks.style.right = "0";
 }
 
-function hideMenu() {
-    navlinks.style.right = "-200px"
+function hideMenu() {;
+    navlinks.style.right = "-200px";
 }
+
+const slider = document.querySelector(".image-comparison .slider");
+const beforeImage = document.querySelector(".image-comparison #image-before");
+
+slider.addEventListener("input", (e) => {
+    console.log("log" + e.target.value)
+    let sliderValue = e.target.value + "%";
+    beforeImage.style.width = sliderValue;
+})
